4ceb785e8af89a8b8aa41cd4e1ebde2c7b1cd88c
eduardapresa <presaeduarda@gmail.com>
2024-10-03 15:10:00 -0300

    Alterei o arquivo

---
+++ script.js
@@ -0,0 +1,13 @@
+function approvedOrNot(score){
+    if(score < 6) return false;
+    return true;
+}
+
+approvedOrNot(5);
+approvedOrNot(7);
+
+const nota = 3;
+
+if (nota<=6){
+    console.log("Você é a vergonha da família")
+}
